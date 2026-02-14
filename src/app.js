32583e3c0ce97f33dd4061f6e6d54cc9f9d4b098
Ampli Game <ampli741@gmail.com>
2026-02-14 19:16:42 +0200

    cleanup app.js and init Phaser properly

--- src/app.js
+++ src/app.js
@@ -1,26 +1,31 @@
 import { startGame } from './game/game.js';
 import { GameScene } from './core/scene.js';
 
-console.log("Taverna War app loaded");
+console.log('Taverna War app loaded');
 
 window.app = {
   player: null,
   mob: null,
   lootActive: false,
-  turn: "PLAYER"
+  turn: 'PLAYER'
 };
 
+// делаем старт игры доступным из консоли / UI
 window.startGame = startGame;
 
-window.initPhaser = function () {
-  new Phaser.Game({
+// инициализация Phaser
+function initPhaser() {
+  const config = {
     type: Phaser.AUTO,
     parent: 'game-container',
     width: 680,
     height: 680,
-    scene: GameScene,
-    transparent: true
-  });
-};
+    backgroundColor: '#1e1e1e',
+    scene: GameScene
+  };
 
-window.initPhaser();
+  new Phaser.Game(config);
+}
+
+// запускаем Phaser сразу
+initPhaser();
