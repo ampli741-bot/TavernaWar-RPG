54cbc0c469288eaf14be28e2f8b86ea6500074d3
Ampli Game <ampli741@gmail.com>
2026-02-15 11:54:35 +0200

    sync project structure

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-console.log('GAME.JS LOADED');
+console.log('GAME.JS REAL FIX LOADED');
 
 // =====================
 // CONFIG
@@ -31,7 +31,7 @@ const GLOW_COLORS = {
 // =====================
 class GameScene extends Phaser.Scene {
   constructor() {
-    super();
+    super({ key: 'GameScene' });
     this.grid = [];
   }
 
@@ -46,16 +46,16 @@ class GameScene extends Phaser.Scene {
   create() {
     const { width, height } = this.scale;
 
-    // BG
+    // ---- BACKGROUND ----
     this.bg = this.add.image(width / 2, height / 2, 'bg');
     const s = Math.max(width / this.bg.width, height / this.bg.height);
     this.bg.setScale(s);
 
-    // OFFSETS
+    // ---- GRID OFFSET ----
     this.OFFSET_X = (width - GRID * TILE_S) / 2;
     this.OFFSET_Y = (height - GRID * TILE_S) / 2;
 
-    // GRID
+    // ---- GRID ----
     for (let r = 0; r < GRID; r++) {
       this.grid[r] = [];
       for (let c = 0; c < GRID; c++) {
@@ -72,61 +72,63 @@ class GameScene extends Phaser.Scene {
 
     const cont = this.add.container(x, y);
 
-    // glow
+    // ---- glow ----
     const glow = this.add.graphics();
     glow.fillStyle(GLOW_COLORS[type], 0.3);
     glow.fillRoundedRect(
-      -VISUAL_S/2-3,
-      -VISUAL_S/2-3,
-      VISUAL_S+6,
-      VISUAL_S+6,
+      -VISUAL_S / 2 - 3,
+      -VISUAL_S / 2 - 3,
+      VISUAL_S + 6,
+      VISUAL_S + 6,
       14
     );
 
-    // bg
+    // ---- bg ----
     const bg = this.add.graphics();
     bg.fillStyle(BG_COLORS[type], 1);
     bg.fillRoundedRect(
-      -VISUAL_S/2,
-      -VISUAL_S/2,
+      -VISUAL_S / 2,
+      -VISUAL_S / 2,
       VISUAL_S,
       VISUAL_S,
       12
     );
 
-    // icon
+    // ---- icon ----
     const img = this.add.image(0, 0, `rune_${type}`);
-    const scale = (type === 'yellow' || type === 'green') ? 1.45 : 1.75;
-    img.setDisplaySize(VISUAL_S * scale, VISUAL_S * scale);
+    const zoom = (type === 'yellow' || type === 'green') ? 1.45 : 1.75;
+    img.setDisplaySize(VISUAL_S * zoom, VISUAL_S * zoom);
 
-    // mask (чтобы спрятать белое)
-    const maskG = this.make.graphics();
+    // ---- MASK (КЛЮЧЕВОЕ ИСПРАВЛЕНИЕ) ----
+    const maskG = this.make.graphics({ add: false });
     maskG.fillStyle(0xffffff);
     maskG.fillRoundedRect(
-      -VISUAL_S/2,
-      -VISUAL_S/2,
+      -VISUAL_S / 2,
+      -VISUAL_S / 2,
       VISUAL_S,
       VISUAL_S,
       12
     );
+    maskG.setPosition(0, 0);
+
     img.setMask(maskG.createGeometryMask());
 
-    // frame
+    // ---- frame ----
     const frame = this.add.graphics();
     frame.lineStyle(5, 0x2b2b2b, 1);
     frame.strokeRoundedRect(
-      -VISUAL_S/2,
-      -VISUAL_S/2,
+      -VISUAL_S / 2,
+      -VISUAL_S / 2,
       VISUAL_S,
       VISUAL_S,
       12
     );
     frame.lineStyle(2, 0xbc962c, 0.8);
     frame.strokeRoundedRect(
-      -VISUAL_S/2+4,
-      -VISUAL_S/2+4,
-      VISUAL_S-8,
-      VISUAL_S-8,
+      -VISUAL_S / 2 + 4,
+      -VISUAL_S / 2 + 4,
+      VISUAL_S - 8,
+      VISUAL_S - 8,
       10
     );
 
