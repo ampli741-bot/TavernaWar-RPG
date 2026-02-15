b21e412afb149393e34828134a8fd69946b7b180
Ampli Game <ampli741@gmail.com>
2026-02-15 08:56:35 +0200

    match-3: remove, gravity, cascade

--- src/app.js
+++ src/app.js
@@ -1,31 +1,14 @@
-import { startGame } from './game/game.js';
 import { GameScene } from './core/scene.js';
 
 console.log('Taverna War app loaded');
 
-window.app = {
-  player: null,
-  mob: null,
-  lootActive: false,
-  turn: 'PLAYER'
+const config = {
+  type: Phaser.AUTO,
+  parent: 'game-container',
+  width: 1024,
+  height: 768,
+  backgroundColor: '#000000',
+  scene: GameScene
 };
 
-// делаем старт игры доступным из консоли / UI
-window.startGame = startGame;
-
-// инициализация Phaser
-function initPhaser() {
-  const config = {
-    type: Phaser.AUTO,
-    parent: 'game-container',
-    width: 680,
-    height: 680,
-    backgroundColor: '#1e1e1e',
-    scene: GameScene
-  };
-
-  new Phaser.Game(config);
-}
-
-// запускаем Phaser сразу
-initPhaser();
+new Phaser.Game(config);
